e54f5151043aa52d118ffe42006cf4a04b6d9362
professorabhay <abhayetwup123@gmail.com>
2025-01-04 06:23:40 +0530

    JWT-Auth_Added

--- src/routers/chatRoutes.js
+++ src/routers/chatRoutes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { getMainChat, sendMessage, startPrivateChat } from "../controllers/chatController.js";
+import { authenticateToken } from "../middlewares/auth.js";
 
 const router = Router();
 
-router.get("/main", getMainChat);
-router.post("/message", sendMessage);
-router.post("/private", startPrivateChat);
+router.get("/main", authenticateToken, getMainChat);
+router.post("/message", authenticateToken, sendMessage);
+router.post("/private", authenticateToken, startPrivateChat);
 
 export default router;
--- src/routers/userRoutes.js
+++ src/routers/userRoutes.js
@@ -1,7 +1,18 @@
 import { Router } from "express";
-import { searchUser } from "../controllers/userController.js";
+import { generateToken } from "../middlewares/auth.js";
 
 const router = Router();
+
+router.post("/login", (req, res) => {
+  const { walletAddress } = req.body;
+
+  if (!walletAddress) {
+    return res.status(400).json({ error: "Wallet address is required." });
+  }
+
+  const token = generateToken(walletAddress);
+  res.status(200).json({ token });
+});
 
 router.get("/search", searchUser);
 
